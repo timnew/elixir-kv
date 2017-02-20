11cb413abd42dd905d0db1d8f9295a2488c6834d
Tim Wen <timnew.wti@gmail.com>
2017-02-20 15:24:26 +1100

    Add routing

--- apps/kv/lib/kv/supervisor.ex
+++ apps/kv/lib/kv/supervisor.ex
@@ -2,13 +2,14 @@ defmodule KV.Supervisor do
   use Supervisor
 
   def start_link do
-    Supervisor.start_link(__MODULE__, :ok)
+    Supervisor.start_link(__MODULE__, :ok, name: KV.Supervisor)
   end
 
   def init(:ok) do
     children = [
       worker(KV.Registry, [KV.Registry]),
-      supervisor(KV.Bucket.Supervisor, [])
+      supervisor(KV.Bucket.Supervisor, []),
+      supervisor(Task.Supervisor, [[name: KV.RouterTasks]])
     ]
 
     supervise(children, strategy: :rest_for_one)
